feeb061ae0fcf61c70ea98c357f8e24890a786bc
Hudson <Simon.Hudson@experian.com>
2020-03-18 10:17:14 +0000

    routes and controllers

---
+++ server/controllers/guest.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const Guest = require('../models/guest');
+
+createGuest = (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a guest',
+        });
+    }
+
+    const guest = new Guest(body);
+
+    if (!guest) {
+        return res.status(400).json({ success: false, error: err });
+    }
+
+    guest
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: guest._id,
+                message: 'Guest created!',
+            });
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Guest not created!',
+            })
+        });
+}
+
+updateGuest = async (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        });
+    }
+
+    Guest.findOne({ _id: req.params.id }, (err, guest) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Guest not found!',
+            });
+        }
+        guest.name = body.name;
+        guest.time = body.time;
+        guest.rating = body.rating;
+        guest
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: guest._id,
+                    message: 'Guest updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Guest not updated!',
+                })
+            });
+    });
+}
+
+deleteGuest = async (req, res) => {
+    await Guest.findOneAndDelete({ _id: req.params.id }, (err, guest) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+
+        if (!guest) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Guest not found` });
+        }
+
+        return res.status(200).json({ success: true, data: guest });
+    }).catch(err => console.log(err));
+}
+
+getGuestById = async (req, res) => {
+    await Guest.findOne({ _id: req.params.id }, (err, guest) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+
+        if (!guest) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Guest not found` });
+        }
+        return res.status(200).json({ success: true, data: guest });
+    }).catch(err => console.log(err));
+}
+
+getGuests = async (req, res) => {
+    await Guest.find({}, (err, guests) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        if (!guests.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Guest not found` });
+        }
+        return res.status(200).json({ success: true, data: guests });
+    }).catch(err => console.log(err));
+}
+
+module.exports = {
+    createGuest,
+    updateGuest,
+    deleteGuest,
+    getGuests,
+    getGuestById,
+}
--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
+    .connect('mongodb://127.0.0.1:27017/wedding', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     });
